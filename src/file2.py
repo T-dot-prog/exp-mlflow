8d2f616dfa464b45b891a130c7aec28ee9c748ab
T-dot-prog <tahasinahoni2@gmail.com>
2025-06-12 21:47:44 +0600

    commited

--- src/file2.py
+++ src/file2.py
@@ -3,9 +3,7 @@ import mlflow.sklearn
 from sklearn.datasets import load_wine
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix
-import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.metrics import accuracy_score
 import os
 
 # Set your DAGsHub credentials
